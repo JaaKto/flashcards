0dcd60dffc8134aabf043aceaea707ae63164d0e
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2020-10-16 23:49:07 +0200

    Updated models

---
+++ server/models/flashcard.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const flashCardSchema = new Schema(
+  {
+    from: {
+      type: String,
+      required: true,
+    },
+    fromLang: {
+      type: String,
+      required: true,
+    },
+    to: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    toLang: {
+      type: String,
+      required: true,
+    },
+    createBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
+
+module.exports = mongoose.model("Flashcard", flashCardSchema)
