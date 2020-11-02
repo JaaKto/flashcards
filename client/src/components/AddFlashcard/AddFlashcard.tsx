a568cd6258d5776766a63100b2b6db6d22ff4868
Jakub Jablecki <33008680+JaaKto@users.noreply.github.com>
2020-11-02 22:20:03 +0100

    Mocked response

--- client/src/components/AddFlashcard/AddFlashcard.tsx
+++ client/src/components/AddFlashcard/AddFlashcard.tsx
@@ -10,20 +10,43 @@ export default () => {
     location: { search, pathname },
   } = useHistory()
 
+  const mockedResponse = {
+    translations: [
+      {
+        source: '<strong class="headword">kotlet</strong>',
+        target:
+          'Kotelett <span class="genus"><acronym title="neuter">nt</acronym></span>',
+      },
+      {
+        source:
+          '<span class="example"><strong class="tilde">kotlet</strong> cielęcy/schabowy</span>',
+        target:
+          'Kalbs-/Schweinskotelett <span class="genus"><acronym title="neuter">nt</acronym></span>',
+      },
+      {
+        source:
+          '<span class="example"><strong class="tilde">kotlet</strong> mielony</span>',
+        target:
+          'B[o]ulette <span class="genus"><acronym title="feminine">f</acronym></span>',
+      },
+    ],
+  }
+
   useEffect(() => {
-    fetchData(pathname.slice(1) + search, {
-      method: "GET",
-      headers: new Headers({
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      }),
-    })
-      .then((response: any) => {
-        setState(response[0][0])
-        console.log(response[0][0])
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    // fetchData(pathname.slice(1) + search, {
+    //   method: "GET",
+    //   headers: new Headers({
+    //     Authorization: `Bearer ${localStorage.getItem("token")}`,
+    //   }),
+    // })
+    //   .then((response: any) => {
+    //     setState(response[0][0])
+    //     console.log(response[0][0])
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
+    setState(mockedResponse)
   }, [])
   return (
     <S.FlashcardsContainer>
